f7518aca27eb3de21bf6e7df798b302af419f178
leaderli <429243408@qq.com>
2023-02-03 12:58:55 +0800

    20230203  125855

--- li/li_decorator.py
+++ li/li_decorator.py
@@ -6,7 +6,7 @@ env = 'uat'
 def run_on_uat(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        print(env)
+        print("----------->",env)
         assert 'pdu' not in env, 'function only support run on uat or local, now is ' + env
         return func(*args, **kwargs)
 
